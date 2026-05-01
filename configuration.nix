6ae7052fa57c8092c4c2d03b027844978289fea9
Sawyer Powell <sawyerhpowell@gmail.com>
2026-04-30 20:18:53 -0700

    create i2c group explicitly

--- configuration.nix
+++ configuration.nix
@@ -107,6 +107,7 @@
   hardware.enableRedistributableFirmware = true;
 
   # DDC/CI: let i2c group members talk to monitors
+  users.groups.i2c = {};
   services.udev.extraRules = ''
     KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
   '';
